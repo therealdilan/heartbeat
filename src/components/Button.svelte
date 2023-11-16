17859ff34bf2f0bb677ee2acdacb773dc21541c8
dilan <125643732+therealdilan@users.noreply.github.com>
2023-11-16 18:22:27 +0400

    testing astro + svelte

---
+++ src/components/Button.svelte
@@ -0,0 +1,14 @@
+<script>
+
+    let message = ''
+
+    export let color
+
+</script>
+
+<button on:click={() => message+= 'hello!'}
+    style="background: {color}; padding: 1rem;">
+    Say hello!
+</button>
+
+{message}
